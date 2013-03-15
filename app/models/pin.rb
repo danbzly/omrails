12f0fea91eaa3eda4961dc625f9779d0ae833ae2
Daniel Beasley <DanBzly@gmail.com>
2013-03-15 17:40:46 -0400

    Reroute root to pins#index

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -6,7 +6,8 @@ class Pin < ActiveRecord::Base
   validates :user_id, presence: true
   validates_attachment :image, presence: true,
   								content_type: {content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] },
-  								size: {less_than: 10.megabytes}
+  								size: {less_than: 100.megabytes}
+    
 
   belongs_to :user
   has_attached_file :image, styles: { medium: "320x240>"}
