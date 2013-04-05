22296ccc5dafc89af315cf310df8f0b1061c9712
Daniel Beasley <DanBzly@gmail.com>
2013-04-04 20:45:32 -0400

    socket error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,18 +64,7 @@ Omrails::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-
   
-
-ActionMailer::Base.smtp_settings = {
-  :address => "danbzly@gmail.com",
-  :port => 25,
-  :domain => "www.limitless-coast-4809.herokuapp.com",
-  :authentication => :login,
-  :user_name => "danbzly@gmail.com",
-  :password => "okokok99"
-}
 
 
   # paperclip shold use Amozon s3 on Heroku
