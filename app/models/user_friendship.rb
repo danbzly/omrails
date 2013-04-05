a8f8abb43e7da2e7bf71020e0ef10e4e79332ed4
Daniel Beasley <DanBzly@gmail.com>
2013-04-04 20:24:08 -0400

    socket

--- app/models/user_friendship.rb
+++ app/models/user_friendship.rb
@@ -35,8 +35,8 @@ validate :not_blocked
 
 def self.request(user1, user2)
     transaction do
-      friendship1 = create(user: user1, friend: user2, state: 'pending')
-      friendship2 = create(user: user2, friend: user1, state: 'requested')
+      friendship1 = create!(user: user1, friend: user2, state: 'pending')
+      friendship2 = create!(user: user2, friend: user1, state: 'requested')
 
       friendship1.send_request_email if !friendship1.new_record?
       friendship1
