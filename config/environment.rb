36769702eb6ce98bd67cd5d443f4ffaa62bf518e
Daniel Beasley <DanBzly@gmail.com>
2013-04-05 13:40:18 -0400

    trying to fix the socket erro adding a different code to enviromet.rb file

--- config/environment.rb
+++ config/environment.rb
@@ -4,12 +4,3 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Omrails::Application.initialize!
 
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :domain               => 'http://limitless-coast-4809.herokuapp.com',
-  :user_name            => 'danbzly@gmail.com',
-  :password             => 'okokok99',
-  :authentication       => 'plain',
-  :enable_starttls_auto => true  }
