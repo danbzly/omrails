0e8aeffd6c8235ca76532e66d46c8cb5781cf6e8
Daniel Beasley <DanBzly@gmail.com>
2013-04-08 11:59:54 -0400

    sendgrid

--- config/initializers/mail_setup.rb
+++
@@ -1,9 +0,0 @@
-ActionMailer::Base.smtp_settings = {
-:address => ‘smtp.sendgrid.net’,
-:port => ’587′,
-:authentication => :plain,
-:user_name => ENV['app13048826@heroku.com'],
-:password => ENV['cutpgevm'],
-:domain => ‘heroku.com’
-}
-ActionMailer::Base.delivery_method = :smtp
