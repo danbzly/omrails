48223205167714594655616c357b2f5be013f19e
Daniel Beasley <DanBzly@gmail.com>
2013-04-08 11:48:50 -0400

    send grid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,15 @@ Omrails::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
   
+  ActionMailer::Base.smtp_settings = {
+:address => ‘smtp.sendgrid.net’,
+:port => ’587′,
+:authentication => :plain,
+:user_name => ENV['app13048826@heroku.com'],
+:password => ENV['cutpgevm'],
+:domain => ‘heroku.com’
+}
+ActionMailer::Base.delivery_method = :smtp
   
 
 
