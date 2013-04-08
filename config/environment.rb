38694c584eae4b03c21332ae7fbfb0ec02f5f5b4
Daniel Beasley <DanBzly@gmail.com>
2013-04-08 12:50:27 -0400

    sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,13 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Omrails::Application.initialize!
 
+
+config.action_mailer.default_url_options = { :host => 'smtp.sendgrid.net' }
+ActionMailer::Base.smtp_settings = {
+  :address        => "smtp.sendgrid.net",
+  :port           => "25",
+  :authentication => :login,
+  :user_name      => ENV['app13048826@heroku.com'],
+  :password       => ENV['cutpgevm'],
+  :domain         => ENV['heroku.com']
+}
