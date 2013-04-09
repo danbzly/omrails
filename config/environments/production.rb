aa669de6fca47a991474b80d8d5587571d4a7c18
danbzly <danbzly@gmail.com>
2013-04-09 09:21:50 -0300

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,9 +79,9 @@ ActionMailer::Base.smtp_settings = {
    config.paperclip_defaults = {
   :storage => :s3,
   :s3_credentials => {
-    :bucket => "entrecommunity",
-    :access_key_id => "AKIAJZNAZU5S4OVM4UOQ",
-    :secret_access_key => "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"
+    :bucket => "",
+    :access_key_id => "",
+    :secret_access_key => ""
   }
 }
  
